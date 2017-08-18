0e7e3b9d821965578808063620dd63c3ebc9f6b1
david.williams <daw@gmx.com>
2017-08-18 09:50:25 +0100

    Added new test

--- txnreport/src/test/java/com/jpm/traderep/txnreport/TradeFileProcessorTest.java
+++ txnreport/src/test/java/com/jpm/traderep/txnreport/TradeFileProcessorTest.java
@@ -6,11 +6,14 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.util.Map;
+import java.util.TreeMap;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jpm.traderep.txnreport.entity.Position;
 import com.jpm.traderep.txnreport.file.input.TradeFileProcessor;
 import com.jpm.traderep.txnreport.file.validation.Validator;
 
@@ -18,82 +21,105 @@ public class TradeFileProcessorTest {
 
 	private TradeFileProcessor tfp;
 	private Validator validator;
+	private Map<Integer, Map<String, Position>> rptDataMap = new TreeMap<>();
 
 	@Before
 	public void setUp() throws Exception {
 
 		tfp = new TradeFileProcessor();
 		validator = new Validator();
-		
+
 		try {
-			tfp.processTxnFile("test.csv", ",");
+			rptDataMap = tfp.processTxnFile("test.csv", ",");
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-
+		for (Integer d : rptDataMap.keySet()) {
+			System.out.println("xx" + rptDataMap.get(d));
+		}
 	}
 
 	@Test
 	public void populateMapTest20160104() {
 
 		String entity = "bar";
-		Integer settleDate=20160104;
-		
+		Integer settleDate = 20160104;
+
+		assertEquals(3, tfp.getTxnDataMap().size()); // Should be an aggregate of 3 rows
+
+		Integer actualSettlementDate = tfp.getTxnDataMap().get(settleDate).get(entity).getActualSettlementDate();
+		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountIncoming();
+		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountOutgoing();
+		assertEquals(actualSettlementDate, new Integer(settleDate));
+		assertEquals(BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_HALF_UP),
+				usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP));
+		assertEquals(new BigDecimal(2155.00).setScale(2, BigDecimal.ROUND_HALF_UP),
+				usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP));
+
+
+	}
+
+	@Test
+	public void populateMapTest20160104Foo() {
+
+		String entity = "foo";
+		Integer settleDate = 20160104;
+
 		assertEquals(tfp.getTxnDataMap().size(), 3); // Should be an aggregate of 3 rows
 
 		Integer actualSettlementDate = tfp.getTxnDataMap().get(settleDate).get(entity).getActualSettlementDate();
-		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity).getUsdSettledAmountIncoming();
-		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity).getUsdSettledAmountOutgoing();
-		assertEquals(actualSettlementDate, new Integer(settleDate));
+		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountIncoming();
+		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountOutgoing();
+		assertEquals(new Integer(settleDate),actualSettlementDate);
+		assertEquals(new BigDecimal(3622.50).setScale(2, BigDecimal.ROUND_HALF_UP),
+				usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP));
 		assertEquals(
-				BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_HALF_UP),usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP));
-		assertEquals(usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP),
-				new BigDecimal(2155.00).setScale(2, BigDecimal.ROUND_HALF_UP));
-		
-//		for (Integer i: tfp.getTxnDataMap().keySet()) {
-//			System.out.println(i + " " + tfp.getTxnDataMap().get(i));
-//		}
-
+				new BigDecimal(10025.00).setScale(2, BigDecimal.ROUND_HALF_UP),usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP));
 	}
-	
- 	@Test
+
+	@Test
 	public void populateMapTest20160107() {
 
 		String entity = "bar";
-		Integer settleDate=20160107;
+		Integer settleDate = 20160107;
 
 		assertEquals(tfp.getTxnDataMap().size(), 3); // Should be an aggregate of 3 rows
 
 		Integer actualSettlementDate = tfp.getTxnDataMap().get(settleDate).get(entity).getActualSettlementDate();
-		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity).getUsdSettledAmountIncoming();
-		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity).getUsdSettledAmountOutgoing();
-		assertEquals(actualSettlementDate, new Integer(settleDate));
-		assertEquals(usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP), new BigDecimal(14899.50).setScale(2, BigDecimal.ROUND_HALF_UP));
-		assertEquals(usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP),BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_HALF_UP));
-	
+		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountIncoming();
+		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountOutgoing();
+		assertEquals(new Integer(settleDate),actualSettlementDate);
+		assertEquals(
+				new BigDecimal(14899.50).setScale(2, BigDecimal.ROUND_HALF_UP),usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP));
+		assertEquals(BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_HALF_UP),usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP));
+
 	}
-	
- 	@Test
+
+	@Test
 	public void populateMapTest20160110() {
 
 		String entity = "bar";
-		Integer settleDate=20160110;
-	
+		Integer settleDate = 20160110;
+
 		assertEquals(tfp.getTxnDataMap().size(), 3); // Should be an aggregate of 3 rows
 
 		Integer actualSettlementDate = tfp.getTxnDataMap().get(settleDate).get(entity).getActualSettlementDate();
-		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity).getUsdSettledAmountIncoming();
-		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity).getUsdSettledAmountOutgoing();
-		assertEquals(actualSettlementDate, new Integer(settleDate));
-		assertEquals(usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP), BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_HALF_UP));
-		assertEquals(usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP),
-				new BigDecimal(14237.30).setScale(2, BigDecimal.ROUND_HALF_UP));
+		BigDecimal usdSettledAmountIncoming = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountIncoming();
+		BigDecimal usdSettledAmountOutgoing = tfp.getTxnDataMap().get(settleDate).get(entity)
+				.getUsdSettledAmountOutgoing();
 		
-//		for (Integer i: tfp.getTxnDataMap().keySet()) {
-//			System.out.println(i + " " + tfp.getTxnDataMap().get(i));
-//		}
-	}
+		assertEquals(new Integer(settleDate),actualSettlementDate);
+		assertEquals( BigDecimal.ZERO.setScale(2, BigDecimal.ROUND_HALF_UP),usdSettledAmountOutgoing.setScale(2, BigDecimal.ROUND_HALF_UP));
+		assertEquals(new BigDecimal(14237.30).setScale(2, BigDecimal.ROUND_HALF_UP), usdSettledAmountIncoming.setScale(2, BigDecimal.ROUND_HALF_UP));
 
+	}
 
 	@Test(expected = IOException.class)
 	public void handleExceptionForNonExistentFile() throws IOException {
@@ -175,7 +201,7 @@ public class TradeFileProcessorTest {
 	public void testInvalidUnits() {
 		assertFalse("Valid Units", validator.validUnits(-1));
 	}
-	
+
 	@After
 	public void tearDown() throws Exception {
 	}
